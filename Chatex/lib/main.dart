f85e73bdaed0cba23240527ca098f39d0cf1a845
szedan20 <datidusz1234@gmail.com>
2025-01-07 10:51:11 +0100

    Chatex 0.2.15
    Firebase Authentication

--- Chatex/lib/main.dart
+++ Chatex/lib/main.dart
@@ -1,8 +1,16 @@
 import 'package:flutter/material.dart';
 import 'widgets/password_visibility.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MaterialApp(home: LoginUI()));
+  await FirebaseAuth.instance.useAuthEmulator('localhost', 9099);
 }
 
 class LoginUI extends StatefulWidget {
